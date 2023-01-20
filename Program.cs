1547d5a074c76a1eaf21e31d089919c33bf33d5e
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-01-20 11:10:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string svar2 = "";
+            string[] svar = { "It is certain", "As I see it, yes", "Reply hazy, try again", "Don't count on it", "It is decidedly so", "Most likely",
+                "Ask again later", "My reply is no", "Better not tell you now", "Very doubtful"};
+            while (svar2 != "a")
+            {
+                Console.WriteLine("Write how many question u want and the Magic 8 ball will give u the answer\nAnd once your done u can write a simple 'a' to leave.");
+                svar2 = Console.ReadLine();
+                Console.WriteLine($"The Magic 8-ball: {svar[random.Next(svar.Length)]}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
