a8d6c6ef9c4f298e70d67a9c6eb34b214f4c0e45
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-01-20 10:07:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            String[] text = { "Jag gillar *Potatos*", "jagg är siiiii", "katchup är bra"};
+
+            foreach (String s in text) 
+            { 
+                Console.WriteLine(s+"\n");
+            }
+        }
+    }
+}
