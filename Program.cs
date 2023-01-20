7d49ab3961b07a3517d825275dad2112c595bec4
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-01-20 10:51:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Jokob", "Elliot", "Johana", "Rongresturant", "Abdi" };
+
+            Array.Sort(namn);
+
+            Console.WriteLine(namn[0]);
+        }
+    }
+}
