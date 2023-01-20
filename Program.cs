d0cbee039a4dfafe3e8862199c28965ecb6ac216
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-01-20 10:14:23 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            int[] tal = new int[antal];
+            Console.WriteLine($"Skriv in {antal} hetal");
+            for (int i = 0; i < tal.Length; i++)
+            {
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Här är talen du skrev in");
+            for (int i = 0; i < tal.Length; i++)
+            { 
+                Console.WriteLine($"Tal {i+1}: {tal[i]}");
+            }
+        }
+    }
+}
